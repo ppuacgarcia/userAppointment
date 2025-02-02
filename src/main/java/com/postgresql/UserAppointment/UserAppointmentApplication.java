81d489fa04684dc8bf4b94c64769ff5cb1a2f550
Pablo David Puac García <pablopuacg@gmail.com>
2025-02-01 18:58:03 -0600

    first commit

---
+++ src/main/java/com/postgresql/UserAppointment/UserAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql.UserAppointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAppointmentApplication.class, args);
+	}
+
+}
